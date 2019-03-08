89efaad5908ff7a03072eaea007f58070a4092ff
MPettersen <mp.markus94@gmail.com>
2019-03-08 13:48:21 +0100

    update

--- wiki_clean.py
+++ wiki_clean.py
@@ -1,21 +1,61 @@
+import numpy as np
+import pandas as pd
+import bz2
 import xml.sax
-import subprocess as sp
+import mwparserfromhell
+import os
+import json
+from time import time
 from wiki_xml_handler import WikiXMLHandler
+from multiprocessing import Pool
 
-wiki = 'D:/DAT500 project/enwiki-20190101-pages-articles-multistream.xml.bz2'
-handler = WikiXMLHandler()
-parser = xml.sax.make_parser()
-parser.setContentHandler(handler)
+wiki_dump = 'C:/data/enwiki-20190220-pages-articles-multistream1.xml-p10p30302.bz2'
+data_folder = 'C:/data/'
+partitions = [data_folder + file for file in os.listdir(data_folder) if 'xml-p']
+
+def preprocess_pages(data_path, save=True):
+    """Finds and cleans all pages from a compressed wikipedia XML file"""
+    start = time()
+    # Object for handling xml
+    handler = WikiXMLHandler()
+
+    # Parsing object
+    parser = xml.sax.make_parser()
+    parser.setContentHandler(handler)
+
+    # Iteratively process file
+    i = 0
+    for line in bz2.BZ2File(data_path, 'r'):
+        try:
+            parser.feed(line)
+        except StopIteration:
+            break
+        i += 1
+        if i > 1e+4: break
+
+    if save:
+        # Save to a file based on the data path name
+        pass
+    
+    end = time()
+    print(f'\n{data_path} preprocessed in {round(end-start)} seconds')
+    print(f'{handler._page_count} pages found in {data_path}')
+    return handler._pages
+
 
 def main():
-    for line in sp.Popen(['bzcat'], 
-                         stdin=open(wiki),
-                         stdout=sp.PIPE).stdout:
-        parser.feed(line)
-        if len(handler.pages) > 2:
-            break
-    
-    print(handler.pages)
+    start = time()
+    # Create a pool of workers to execute processes
+    pool = Pool(processes = 4)
+
+    # Map (service, task), applies function to each partition 
+    results = pool.map(preprocess_pages, partitions)
+
+    pool.close()
+    pool.join()
+    end = time()
+    print(f'\nWhole dump preprocessed in {round(end-start)} seconds')
+    print(f'\nLength of results {len(results[-1])}')
 
 
 if __name__ == '__main__':
--- wiki_xml_handler.py
+++ wiki_xml_handler.py
@@ -1,32 +1,67 @@
 import xml.sax
-import subprocess
+import mwparserfromhell
 
 class WikiXMLHandler(xml.sax.handler.ContentHandler):
-    """Content hadler for Wiki XML data using SAX"""
+    """Content handler for Wiki XML data using SAX"""
     def __init__(self):
         xml.sax.handler.ContentHandler.__init__(self)
-        self.__buffer = None
-        self.__values = {}
-        self.__current_tag = None
-        self.pages = []
-
-
-    def __characters(self, content):
+        self._buffer = None
+        self._values = {}
+        self._current_tag = None
+        self._previous_tag = None
+        self._pages = []
+        
+        
+    def characters(self, content):
         """Characters between opening and closing tags"""
-        if self.__current_tag:
-            self.__buffer.append(content)
-
-
-    def __startElement(self, name):
+        if self._current_tag:
+            self._buffer.append(content)
+            
+            
+    def startElement(self, name, attrs):
         """Opening tag of element"""
-        if name in ('title', 'text'):
-            self.__current_tag = name
-            self.__buffer = []
-    
-
-    def __endElement(self, name):
+        if name in ('id', 'title', 'text'):
+            self._previous_tag = self._current_tag
+            self._current_tag = name
+            self._buffer = []
+            
+        
+    def endElement(self, name):
         """Closing tag of element"""
-        if name == self.__current_tag:
-            self.__values[name] = ' '.join(self.__buffer)
+        if name == self._current_tag:
+            if name == 'text':
+                self._process_page()
+            elif name == 'id' and self._previous_tag == 'id':
+                pass
+            else:
+                self._values[name] = ' '.join(self._buffer)
         if name == 'page':
-            self.pages.append((self.__values['title'], self.__values['text']))
+            if not self._redirect():
+                self._pages.append((self._values['id'],
+                                    self._values['title'],
+                                    self._values['text'],
+                                    self._values['wikilinks'],
+                                    self._values['extlinks']))
+                self._page_count = len(self._pages)
+    
+    
+    def _redirect(self):
+        """Checking if the page is a redirect page or not"""
+        wiki = mwparserfromhell.parse(self._values['text'])
+        text = wiki.strip_code().split()
+        if len(text) == 0:
+            return False
+        return text[0] == 'REDIRECT'
+    
+    
+    def _process_page(self):
+        """
+        Processing the text and retrieving the internal wikilinks and the
+        external url-links.
+        """
+        content = mwparserfromhell.parse(self._buffer)
+        content = content.strip_code().strip()
+        content = mwparserfromhell.parse(content)
+        self._values['text'] = content.strip_code().strip()
+        self._values['wikilinks'] = [x.title.strip_code() for x in content.filter_wikilinks()]
+        self._values['extlinks'] = [x.url.strip_code().strip() for x in content.filter_external_links()]
